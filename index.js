28b29fb2494acbe8781734d43508672c0e907875
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-02 21:10:46 +0900

    auth 기능구현

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 const { User } = require("./models/User");
-
+const { auth } = require("./middleware/auth");
 // application/x-www-form-urlencoded 라는걸 클라이언트에서 받아와서 분석하기 위해 넣어준것
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json 이라는걸 클라이언트에서 받아와서 분석하기 위해 넣어준것
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 const mongoose = require("mongoose");
+
 mongoose
   .connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -29,7 +30,7 @@ const handleListening = () => {
 
 app.get("/", (req, res) => res.send("hello World!!!!"));
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원 가입 할때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
 
@@ -44,7 +45,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -75,5 +76,25 @@ app.post("/login", (req, res) => {
     });
   });
 });
+
+
+app.get('/api/users/auth', auth , (req, res) => { // 중간에 있는 auth는 콜백하기전에 해주는 작업임 (미들웨어)
+
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+
+
+
 
 app.listen(port, handleListening);
