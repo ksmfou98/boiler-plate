008a01e84ad8e22d2094a5d33d97851f0d753df2
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-07 10:57:45 +0900

    css 라이브러리 antd 설치

--- server/config/key.js
+++ server/config/key.js
@@ -3,3 +3,9 @@ if (process.env.NODE_ENV === "production") {
 } else {
   module.exports = require("./dev");
 }
+
+// dev.js 생성후 추가
+// module.exports = {
+//   mongoURI:
+//     "mongodb+srv://ksmfou98:비밀번호-@bolierplate.4jsmk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+// };
