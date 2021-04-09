699dca02854b0fd166bf43852d1c27b1dc414ea4
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-09 15:20:51 +0900

    LoginPage 스타일링

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,7 +6,11 @@ const LandingPage = () => {
       console.log(response);
     });
   }, []);
-  return <div>landingPage</div>;
+  return (
+    <div style={{display:'flex', justifyContent: 'center', alignItems: 'center', width:'100%', height:'100vh'}}>    
+      <h2>시작페이지</h2>
+    </div>
+  );
 };
 
 export default LandingPage;
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,11 +1,53 @@
-import React from "react"
+import React, { useState } from "react";
 
 const LoginPage = () => {
-    return(
-        <div>
-            LoginPage
-        </div>
-    )
-}
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-export default LoginPage
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
+  };
+
+  const { email, password } = form;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form action="" style={{ display: "flex", flexDirection: "column" }}>
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          value={email}
+          onChange={onChange}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={password}
+          onChange={onChange}
+        />
+
+        <button type="submit">로그인</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
