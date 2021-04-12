0ed0a84af8d736a44257c15ef27b749023f7cc3e
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-12 13:49:56 +0900

    로그아웃 기능  구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,37 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-const LandingPage = () => {
+import { withRouter } from "react-router-dom";
+const LandingPage = (props) => {
   useEffect(() => {
     axios.get("/api/hello").then((response) => {
       console.log(response);
     });
   }, []);
+
+
+  const onLogout = () => {
+    axios.get('/api/users/logout')
+      .then(response => {
+        if (response.data.success) {
+          props.history.push("/login")
+        }
+        else{
+          alert("로그아웃에 실패했습니다")
+        }
+      })
+  }
+
+
+
   return (
     <div style={{display:'flex', justifyContent: 'center', alignItems: 'center', width:'100%', height:'100vh'}}>    
       <h2>시작페이지</h2>
+
+      <button onClick={onLogout}>
+        로그아웃
+      </button>
     </div>
   );
 };
 
-export default LandingPage;
+export default withRouter(LandingPage);
