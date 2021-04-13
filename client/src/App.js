0156f81825701ff0d036dbcfeed359745479d8df
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-13 10:55:00 +0900

    Implements : 각 계정 인증 체크

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,16 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import Auth from "./hoc/auth"
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/">
-            <LandingPage />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/register">
-            <RegisterPage />
-          </Route>
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route path="/login" component={Auth(LoginPage, false)} />
+          <Route path="/register" component={Auth(RegisterPage, false)} />
         </Switch>
       </div>
     </Router>
--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_USER, REGISTER_USER } from "../_actions/types";
+import { AUTH_USER, LOGIN_USER, REGISTER_USER } from "../_actions/types";
 
 export default function (state = {}, action) {
   switch (action.type) {
@@ -7,7 +7,10 @@ export default function (state = {}, action) {
       break;
 
     case REGISTER_USER:
-      return { ...state, register: action.payload }
+      return { ...state, register: action.payload };
+      break;
+    case AUTH_USER:
+      return { ...state, userData: action.payload };
       break;
     default:
       return state;
